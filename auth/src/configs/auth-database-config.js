35237fe3d339c4b04e64a646d7ef210456513edc
dineshranjan2001 <dineshranjanbiswal70@gmail.com>
2024-03-16 11:05:09 +0530

    remove some dependencies and update the modules

--- auth/src/configs/auth-database-config.js
+++ auth/src/configs/auth-database-config.js
@@ -1,14 +1,21 @@
 const  {Sequelize}=require("sequelize");
+const database_config=require("../test/mysql_config.json");
 
 
 
-console.log(process.env,process.env.MYSQL_DATABASE,process.env.HOST,process.env.MYSQL_ROOT_PASSWORD,process.env.DB_USER)
+// console.log(process.env,process.env.MYSQL_DATABASE,process.env.HOST,process.env.MYSQL_ROOT_PASSWORD,process.env.DB_USER)
 
 const DATABASE_CONNECTION_CONFIG=new Sequelize(process.env.MYSQL_DATABASE,process.env.DB_USER,process.env.MYSQL_ROOT_PASSWORD,{
     host:process.env.HOST,
     port:process.env.PORT, 
     dialect:"mysql", 
 });  
+// const DATABASE_CONNECTION_CONFIG=new Sequelize(database_config.database,database_config.username,database_config.password,{
+//     host:database_config.host,
+//     port:database_config.port, 
+//     dialect:database_config.dialect,
+//     logging:false,
+// });  
   
 (async ()=>{
     try {
@@ -18,6 +25,7 @@ const DATABASE_CONNECTION_CONFIG=new Sequelize(process.env.MYSQL_DATABASE,proces
         console.log("Database is synced"); 
     } catch (error) {
         console.log("Unable to connect to database due to ",error); 
+        throw error;
     }
 })();
 
